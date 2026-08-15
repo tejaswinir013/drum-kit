56868fffb6018f76e6afb1c9bf148b2d0697ddce
tejaswinir013 <tejaswinir013@gmail.com>
2026-08-15 22:00:07 +0530

    Add Drum Kit project

---
+++ index.js
@@ -0,0 +1,74 @@
+var buttons = document.querySelectorAll(".drum");
+for(var i = 0; i<buttons.length; i++)
+{
+    buttons[i].addEventListener("click",function()
+{
+
+    var drums = this.innerHTML;
+    makeSound(drums);
+    buttonAnimation(drums);
+} );
+
+}
+
+document.addEventListener("keypress", function(event){
+   makeSound(event.key);
+   buttonAnimation(drums);
+});
+
+function makeSound(key)
+{
+    switch (key) 
+    {
+    case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+    
+        case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+        case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+        case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+        case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+        case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+    default:
+        break;
+   }
+}
+
+function buttonAnimation(currentkey)
+{
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+    activeButton.classList.remove("pressed");
+    }, 100);
+    
+}
+
+
+
+
